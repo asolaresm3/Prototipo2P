d50f37a025e2cf257fad02ac466cdd18924b0140
asolaresm3 <52753087+asolaresm3@users.noreply.github.com>
2020-04-29 12:19:50 -0600

    Mantenimientos

---
+++ Examen_P2/Capa_Logica/Logica.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Datos;
+
+namespace Capa_Logica
+{
+    public class Logica
+    {
+        Sentencias sn = new Sentencias();
+        /*-----------------------------------------------------------------------Metodos Generales------------------------------------------------------------*/
+        public string siguiente(string tabla, string campo)
+        {
+            string llave = sn.obtenerfinal(tabla, campo);
+            return llave;
+        }
+
+        public OdbcDataReader Insertar(string[] datos)
+        {
+            return sn.Insertar(datos);
+        }
+        public OdbcDataReader Eliminar(string[] datos)
+        {
+            return sn.Eliminar(datos);
+        }
+
+        public OdbcDataReader Modificar(string[] datos, string[] campos)
+        {
+            return sn.Modificar(datos, campos);
+        }
+
+        public OdbcDataReader consultarPuesto()
+        {
+            return sn.consultaPuesto();
+        }
+
+        public OdbcDataReader consultarConcepto()
+        {
+            return sn.consultaConcepto();
+        }
+
+        public OdbcDataReader consultarDepartamento()
+        {
+            return sn.consultaDepartamento();
+        }
+    }
+}
